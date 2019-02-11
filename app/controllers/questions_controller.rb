58a3596de6fa8ada390bf0bd7de7cdbc1f60266e
Pieter-Jan Caals <Pieter.Jan.Caals@gmail.com>
2019-02-11 13:05:57 -0500

    Implemented basic behaviour

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,15 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    raise
+    @question = params[:question]
+    @answer = ''
+
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question.last == '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
